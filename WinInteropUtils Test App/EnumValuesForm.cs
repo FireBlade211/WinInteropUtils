48f73764e2310fe089fdc35a2629e4fa72d8dd59
SpectralBlade <megajustdancer@gmail.com>
2025-08-23 16:22:14 +0200

    Added (unfinished) test app

---
+++ WinInteropUtils Test App/EnumValuesForm.cs
@@ -0,0 +1,342 @@
+﻿using FireBlade.WinInteropUtils;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace WinInteropUtils_Test_App
+{
+    public partial class EnumValuesForm : Form
+    {
+        private TaskDialogProgressBar? progressBar;
+        private TaskDialogPage? loadingPage;
+        private ManualResetEventSlim? pauseEvent;
+
+        public EnumValuesForm()
+        {
+            InitializeComponent();
+
+            var asm = Assembly.GetAssembly(typeof(COM));
+            if (asm != null)
+            {
+                comboBox1.BeginUpdate();
+                foreach (var Enum in asm.GetTypes().Where(t => t.IsEnum))
+                {
+                    comboBox1.Items.Add(new EnumComboBoxItem(Enum));
+                }
+                comboBox1.SelectedIndex = 0;
+                comboBox1.EndUpdate();
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            progressBar = new(TaskDialogProgressBarState.Normal);
+
+            var btnPause = new TaskDialogButton
+            {
+                Text = "Pause",
+                AllowCloseDialog = false
+            };
+
+            var page = new TaskDialogPage
+            {
+                Heading = "Loading Values",
+                Buttons = [], // Pause button is kind of broken, so it got removed
+                Caption = "Loading Enum Values",
+                ProgressBar = progressBar,
+                Text = "Loading the enum values...",
+                Icon = TaskDialogIcon.ShieldBlueBar
+            };
+
+            page.Created += (s, e) =>
+            {
+                TaskDialog? dialog = page?.BoundDialog;
+                if (dialog != null)
+                {
+                    IntPtr hwnd = dialog.Handle;
+                    try
+                    {
+                        // We can update the icon using a SendMessage call. But we must specify the icon via ID, not an object or hIcon handle
+                        // We do NOT use the negative of the ID, since the API is doing other stuff with the ID and handles it automatically
+                        User32.SendMessage(hwnd, (uint)TDM.UPDATE_ICON, UIntPtr.Zero, new IntPtr(5335));
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error updating icon: " + ex.Message);
+                    }
+
+                }
+            };
+
+            btnPause.Click += (s, e) =>
+            {
+                if (pauseEvent != null)
+                {
+                    if (pauseEvent.IsSet)
+                    {
+                        pauseEvent.Reset();
+                        progressBar.State = TaskDialogProgressBarState.Paused;
+                        btnPause = btnPause.SetButtonText("Resume");
+                    }
+                    else
+                    {
+                        pauseEvent.Set();
+                        progressBar.State = TaskDialogProgressBarState.Normal;
+                        btnPause = btnPause.SetButtonText("Pause");
+                    }
+                }
+            };
+
+            pauseEvent = new(true);
+
+            backgroundWorker1.RunWorkerAsync();
+
+            loadingPage = page;
+            TaskDialog.ShowDialog(this, page, TaskDialogStartupLocation.CenterScreen);
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            object? comboBoxItem = null;
+            if (InvokeRequired)
+            {
+                Invoke(() =>
+                {
+                    comboBoxItem = comboBox1.SelectedItem;
+                });
+            }
+            else
+            {
+                comboBoxItem = comboBox1.SelectedItem;
+            }
+
+            if (comboBoxItem is EnumComboBoxItem ecbi)
+            {
+                try
+                {
+                    if (InvokeRequired)
+                    {
+                        Invoke(() =>
+                        {
+                            listView1.Items.Clear();
+                            imageList1.Images.Clear();
+                        });
+                    }
+                    else
+                    {
+                        listView1.Items.Clear();
+                        imageList1.Images.Clear();
+                    }
+
+                    var vals = Enum.GetValues(ecbi.Enum);
+
+                    int i = 0;
+
+                    foreach (var enumItem in vals)
+                    {
+                        pauseEvent?.Wait(-1);
+                        if (enumItem != null)
+                        {
+                            var icon = enumItem.ToString() switch
+                            {
+                                string s when s.StartsWith("S_") || s.Contains("SUCCESS_") || s.Contains("_S_") || s.Contains("_I_") => SystemIcons.Information,
+                                string s when s.StartsWith("E_") || s.Contains("ERROR_") || s.Equals("CloseButton", StringComparison.OrdinalIgnoreCase) ||
+                                s.Contains("_E_") => SystemIcons.Error,
+                                string s when s.Contains("_W_") || s.Contains("WARNING_") ||
+                                s.Equals("Reserved", StringComparison.OrdinalIgnoreCase) => SystemIcons.Warning,
+                                string s when s.Equals("HelpButton") => SystemIcons.Question,
+                                _ => null
+                            };
+
+                            var item = new ListViewItem
+                            {
+                                Text = enumItem.ToString(),
+                                Tag = ecbi,
+                                SubItems =
+                                {
+                                    new ListViewItem.ListViewSubItem
+                                    {
+                                        Text = ((int)enumItem).ToString("X")
+                                    }
+                                }
+                            };
+
+                            if (icon != null)
+                            {
+                                if (InvokeRequired)
+                                {
+                                    Invoke(() =>
+                                    {
+                                        imageList1.Images.Add(icon);
+                                    });
+                                }
+                                else
+                                {
+                                    imageList1.Images.Add(icon);
+                                }
+                                item.ImageIndex = imageList1.Images.Count - 1;
+                            }
+                            else
+                            {
+                                var image = enumItem.ToString() switch
+                                {
+                                    string s when s.Equals("TitleBar", StringComparison.OrdinalIgnoreCase) => Properties.Resources.titlebar,
+                                    string s when s.Equals("MaximizeButton", StringComparison.OrdinalIgnoreCase) => Properties.Resources.titlebar_max,
+                                    string s when s.Equals("MinimizeButton", StringComparison.OrdinalIgnoreCase) => Properties.Resources.titlebar_min,
+                                    _ => null
+                                };
+
+                                if (image != null)
+                                {
+                                    if (InvokeRequired)
+                                    {
+                                        Invoke(() =>
+                                        {
+                                            imageList1.Images.Add(image);
+                                        });
+                                    }
+                                    else
+                                    {
+                                        imageList1.Images.Add(image);
+                                    }
+                                    item.ImageIndex = imageList1.Images.Count - 1;
+                                }
+                            }
+
+                            if (InvokeRequired)
+                            {
+                                Invoke(() =>
+                                {
+                                    listView1.Items.Add(item);
+                                });
+                            }
+                            else
+                            {
+                                listView1.Items.Add(item);
+                            }
+
+                            backgroundWorker1.ReportProgress(i / vals.Length * 100);
+
+                            i++;
+                        }
+                    }
+                }
+                catch (ObjectDisposedException)
+                { }
+                catch (Exception ex)
+                {
+                    e.Result = ex;
+                }
+            }
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            if (progressBar != null)
+            {
+                try
+                {
+                    progressBar.Value = e.ProgressPercentage;
+                }
+                catch { }
+
+            }
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Result is Exception ex)
+            {
+                throw ex;
+            }
+            else
+            {
+                loadingPage?.BoundDialog?.Close();
+            }
+            pauseEvent?.Set();
+            pauseEvent?.Dispose();
+            pauseEvent = null;
+        }
+    }
+
+    public class EnumComboBoxItem(Type e)
+    {
+        public Type Enum { get; set; } = e;
+
+        public override string ToString()
+        {
+            return Enum.Name;
+        }
+    }
+
+    public enum TDM : uint
+    {
+        NAVIGATE_PAGE = WM.USER + 101,
+        /// <summary>
+        ///   "wParam = Button ID"
+        /// </summary>
+        CLICK_BUTTON = WM.USER + 102,
+        /// <summary>
+        ///   "wParam = 0 (nonMarque) wParam != 0 (Marquee)"
+        /// </summary>
+        SET_MARQUEE_PROGRESS_BAR = WM.USER + 103,
+        /// <summary>
+        ///   "wParam = new progress state"
+        /// </summary>
+        SET_PROGRESS_BAR_STATE = WM.USER + 104,
+        /// <summary>
+        ///   "lParam = MAKELPARAM(nMinRange, nMaxRange)"
+        /// </summary>
+        SET_PROGRESS_BAR_RANGE = WM.USER + 105,
+        /// <summary>
+        ///   "wParam = new position"
+        /// </summary>
+        SET_PROGRESS_BAR_POS = WM.USER + 106,
+        /// <summary>
+        ///   "wParam = 0 (stop marquee), wParam != 0 (start marquee),
+        ///   lparam = speed (milliseconds between repaints)"
+        /// </summary>
+        SET_PROGRESS_BAR_MARQUEE = WM.USER + 107,
+        /// <summary>
+        ///   "wParam = element (TASKDIALOG_ELEMENTS), lParam = new element text (LPCWSTR)"
+        /// </summary>
+        SET_ELEMENT_TEXT = WM.USER + 108,
+        /// <summary>
+        ///   "wParam = Radio Button ID"
+        /// </summary>
+        CLICK_RADIO_BUTTON = WM.USER + 110,
+        /// <summary>
+        ///   "lParam = 0 (disable), lParam != 0 (enable), wParam = Button ID"
+        /// </summary>
+        ENABLE_BUTTON = WM.USER + 111,
+        /// <summary>
+        ///   "lParam = 0 (disable), lParam != 0 (enable), wParam = Radio Button ID"
+        /// </summary>
+        ENABLE_RADIO_BUTTON = WM.USER + 112,
+        /// <summary>
+        ///   "wParam = 0 (unchecked), 1 (checked), lParam = 1 (set key focus)"
+        /// </summary>
+        CLICK_VERIFICATION = WM.USER + 113,
+        /// <summary>
+        ///   "wParam = element (TASKDIALOG_ELEMENTS), lParam = new element text (LPCWSTR)"
+        /// </summary>
+        UPDATE_ELEMENT_TEXT = WM.USER + 114,
+        /// <summary>
+        ///   "wParam = Button ID, lParam = 0 (elevation not required),
+        ///   lParam != 0 (elevation required)"
+        /// </summary>
+        SET_BUTTON_ELEVATION_REQUIRED_STATE = WM.USER + 115,
+        /// <summary>
+        ///   "wParam = icon element (TASKDIALOG_ICON_ELEMENTS), lParam = new icon
+        ///   (hIcon if TDF_USE_HICON_* was set, PCWSTR otherwise)"
+        /// </summary>
+        UPDATE_ICON = WM.USER + 116,
+    }
+
+    public enum WM : uint
+    {
+        USER = 0x0400,
+        WM_SETICON = 0x0080,
+    }
+}
---
+++ WinInteropUtils Test App/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace WinInteropUtils_Test_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            splitContainer1 = new SplitContainer();
+            label1 = new Label();
+            listView1 = new ListView();
+            columnHeader1 = new ColumnHeader();
+            label2 = new Label();
+            propertyGrid1 = new PropertyGrid();
+            menuStrip1 = new MenuStrip();
+            menuToolStripMenuItem = new ToolStripMenuItem();
+            callMethodToolStripMenuItem = new ToolStripMenuItem();
+            toolStripMenuItem1 = new ToolStripSeparator();
+            settingsToolStripMenuItem = new ToolStripMenuItem();
+            exitToolStripMenuItem = new ToolStripMenuItem();
+            viewToolStripMenuItem = new ToolStripMenuItem();
+            viewHRESULTValuesToolStripMenuItem = new ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.Panel2.SuspendLayout();
+            splitContainer1.SuspendLayout();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            splitContainer1.Dock = DockStyle.Fill;
+            splitContainer1.Location = new Point(0, 24);
+            splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.Controls.Add(label1);
+            splitContainer1.Panel1.Controls.Add(listView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            splitContainer1.Panel2.Controls.Add(label2);
+            splitContainer1.Panel2.Controls.Add(propertyGrid1);
+            splitContainer1.Size = new Size(709, 510);
+            splitContainer1.SplitterDistance = 344;
+            splitContainer1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            label1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 6);
+            label1.Name = "label1";
+            label1.Size = new Size(57, 15);
+            label1.TabIndex = 6;
+            label1.Text = "Methods:";
+            // 
+            // listView1
+            // 
+            listView1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            listView1.Columns.AddRange(new ColumnHeader[] { columnHeader1 });
+            listView1.FullRowSelect = true;
+            listView1.Location = new Point(12, 24);
+            listView1.MultiSelect = false;
+            listView1.Name = "listView1";
+            listView1.Size = new Size(310, 474);
+            listView1.TabIndex = 3;
+            listView1.UseCompatibleStateImageBehavior = false;
+            listView1.View = View.Details;
+            listView1.SelectedIndexChanged += listView1_SelectedIndexChanged;
+            // 
+            // columnHeader1
+            // 
+            columnHeader1.Text = "Name";
+            columnHeader1.Width = 300;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(16, 6);
+            label2.Name = "label2";
+            label2.Size = new Size(69, 15);
+            label2.TabIndex = 5;
+            label2.Text = "Arguments:";
+            // 
+            // propertyGrid1
+            // 
+            propertyGrid1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            propertyGrid1.Location = new Point(16, 24);
+            propertyGrid1.Name = "propertyGrid1";
+            propertyGrid1.Size = new Size(326, 474);
+            propertyGrid1.TabIndex = 4;
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { menuToolStripMenuItem, viewToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(709, 24);
+            menuStrip1.TabIndex = 5;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // menuToolStripMenuItem
+            // 
+            menuToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { callMethodToolStripMenuItem, toolStripMenuItem1, settingsToolStripMenuItem, exitToolStripMenuItem });
+            menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            menuToolStripMenuItem.Size = new Size(52, 20);
+            menuToolStripMenuItem.Text = "Home";
+            // 
+            // callMethodToolStripMenuItem
+            // 
+            callMethodToolStripMenuItem.Enabled = false;
+            callMethodToolStripMenuItem.Name = "callMethodToolStripMenuItem";
+            callMethodToolStripMenuItem.ShortcutKeys = Keys.F2;
+            callMethodToolStripMenuItem.Size = new Size(162, 22);
+            callMethodToolStripMenuItem.Text = "Call Method";
+            callMethodToolStripMenuItem.Click += callMethodToolStripMenuItem_Click;
+            // 
+            // toolStripMenuItem1
+            // 
+            toolStripMenuItem1.Name = "toolStripMenuItem1";
+            toolStripMenuItem1.Size = new Size(159, 6);
+            // 
+            // settingsToolStripMenuItem
+            // 
+            settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            settingsToolStripMenuItem.ShortcutKeyDisplayString = "Ctrl+,";
+            settingsToolStripMenuItem.ShortcutKeys = Keys.Control | Keys.Oemcomma;
+            settingsToolStripMenuItem.Size = new Size(162, 22);
+            settingsToolStripMenuItem.Text = "Settings...";
+            settingsToolStripMenuItem.Click += settingsToolStripMenuItem_Click;
+            // 
+            // exitToolStripMenuItem
+            // 
+            exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            exitToolStripMenuItem.ShortcutKeys = Keys.Alt | Keys.F4;
+            exitToolStripMenuItem.Size = new Size(162, 22);
+            exitToolStripMenuItem.Text = "Exit";
+            exitToolStripMenuItem.Click += exitToolStripMenuItem_Click;
+            // 
+            // viewToolStripMenuItem
+            // 
+            viewToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { viewHRESULTValuesToolStripMenuItem });
+            viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            viewToolStripMenuItem.Size = new Size(44, 20);
+            viewToolStripMenuItem.Text = "View";
+            // 
+            // viewHRESULTValuesToolStripMenuItem
+            // 
+            viewHRESULTValuesToolStripMenuItem.Name = "viewHRESULTValuesToolStripMenuItem";
+            viewHRESULTValuesToolStripMenuItem.Size = new Size(178, 22);
+            viewHRESULTValuesToolStripMenuItem.Text = "View Enum Values...";
+            viewHRESULTValuesToolStripMenuItem.Click += viewHRESULTValuesToolStripMenuItem_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(709, 534);
+            Controls.Add(splitContainer1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "WinInteropUtils Test App";
+            splitContainer1.Panel1.ResumeLayout(false);
+            splitContainer1.Panel1.PerformLayout();
+            splitContainer1.Panel2.ResumeLayout(false);
+            splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+            splitContainer1.ResumeLayout(false);
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private SplitContainer splitContainer1;
+        private ListView listView1;
+        private ColumnHeader columnHeader1;
+        private Label label2;
+        private PropertyGrid propertyGrid1;
+        private Label label1;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem menuToolStripMenuItem;
+        private ToolStripMenuItem callMethodToolStripMenuItem;
+        private ToolStripMenuItem viewToolStripMenuItem;
+        private ToolStripMenuItem viewHRESULTValuesToolStripMenuItem;
+        private ToolStripSeparator toolStripMenuItem1;
+        private ToolStripMenuItem settingsToolStripMenuItem;
+        private ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
