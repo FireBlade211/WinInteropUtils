de509f589abe25c89bda62feb5c17f46362e7f74
SpectralBlade <megajustdancer@gmail.com>
2025-09-07 15:03:28 +0200

    Added DocFX files

--- WinInteropUtils/ErrorDiagIDs.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FireBlade.WinInteropUtils
-{
-    internal static class ErrorDiagIDs
-    {
-        public const string IdPrefix = "WIU";
-
-        public const string ReservedEnumValue = IdPrefix + "128";
-    }
-}
